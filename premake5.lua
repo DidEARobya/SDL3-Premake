948e3d1d68de481c86b2b11023bf8e93dc057d00
Ben Higham <94679239+DidEARobya@users.noreply.github.com>
2025-08-09 20:23:23 +0100

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,107 +1,31 @@
-workspace "NoctalEngine"
-	architecture "x64"
-	startproject "Sandbox"
-
-	configurations
-	{
-		"Debug",
-		"Release",
-		"Dist"
-	}
-
-	outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
-
-project "NoctalEngine"
-	location "NoctalEngine"
+project "SDL3"
 	kind "SharedLib"
 	language "C++"
-	buildoptions{"/utf-8"}
+	staticruntime "off"
+	warnings "off0"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("int/" .. outputdir .. "/%{prj.name}")
 
-	pchheader "nepch.h"
-	pchsource "NoctalEngine/src/nepch.cpp"
-
 	files
 	{
-		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp"
-	}
-
-	includedirs
-	{
-		"%{prj.name}/src",
-		"%{prj.name}/vendor/spdlog/include"
+		"include/SDL3/SDL.h",
+		"src/audio",
+		"src/events",
+		"src/gpu",
+		"src/render",
+		"src/video"
 	}
 
 	filter "system:windows"
-		cppdialect "C++latest"
-		staticruntime "On"
-		systemversion "latest"
-
-		defines
-		{
-			"NE_PLATFORM_WINDOWS",
-			"NE_BUILD_DLL"
-		}
-
-		postbuildcommands
-		{
-			("{COPYFILE} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
-		}
-
-	filter "configurations.Debug"
-		defines "NE_DEBUG"
+		runtime "Debug"
 		symbols "On"
+		sanitize {"Address"}
+		flags {"NoRuntimeChecks", "NoIncrementalLink"}
 
-	filter "configurations.Release"
-		defines "NE_RELEASE"
-		optimize "On"
-
-	filter "configurations.Dist"
-		defines "NE_DIST"
-		optimize "On"
-
-
-project "Sandbox"
-	location "Sandbox"
-	kind "ConsoleApp"
-	language "C++"
-	buildoptions{"/utf-8"}
-
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("int/" .. outputdir .. "/%{prj.name}")
-
-	files
-	{
-		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp"
-	}
-
-	includedirs
-	{
-		"NoctalEngine/src",
-		"NoctalEngine/vendor/spdlog/include"
-	}
-
-	links
-	{
-		"NoctalEngine"
-	}
-
-	filter "system:windows"
-		cppdialect "C++latest"
-		staticruntime "On"
-		systemversion "latest"
-
-		defines
-		{
-			"NE_PLATFORM_WINDOWS"
-		}
 
 	filter "configurations.Debug"
-		defines "NE_DEBUG"
+		defines "Debug"
 		symbols "On"
 
 	filter "configurations.Release"
